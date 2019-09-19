715a3342bef61eb06c68cb6a4bae2d79988168ea
jAi <jAi@DESKTOP>
2019-09-19 23:24:52 +0530

    ewd

--- Selenium1/src/locators/Seleniumpoint1.java
+++ Selenium1/src/locators/Seleniumpoint1.java
@@ -13,9 +13,5 @@ public class Seleniumpoint1 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://seleniumpoint.com");
 		System.out.println("website loaded successfully");
-		/*driver.findElement(By.id("name")).sendKeysss("name1");
-		driver.findElement(By.name("umobile")).sendKeys("12345");
-	
-		*/
-	}		
+			}		
 	}
