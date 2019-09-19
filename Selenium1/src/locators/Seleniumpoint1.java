2329df8987a4d80822c76912ab125513913b4038
jAi <jAi@DESKTOP>
2019-09-19 23:02:26 +0530

    new project

---
+++ Selenium1/src/locators/Seleniumpoint1.java
@@ -0,0 +1,21 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleniumpoint1 {
+	
+		public static void main(String[] agrs){
+		//public void locators(){
+		
+		System.setProperty("webdriver.chrome.driver", "C://drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://seleniumpoint.com");
+		System.out.println("website loaded successfully");
+		/*driver.findElement(By.id("name")).sendKeys("name1");
+		driver.findElement(By.name("umobile")).sendKeys("12345");
+		driver.findElement(By.linkText("terms")).click();
+		*///driver.findElement(By.id("name")).sendKeys("name1");
+	}		
+	}
